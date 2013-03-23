d29a7283a87cf166f4b9741ffdb97f66fc79fdea
scaret <lyc945@gmail.com>
2013-03-23 22:30:46 +0800

    移除了版本过渡中的不必要的库依赖，顺手改了些相关的和无关紧要的代码

--- js/yssywall.js
+++ js/yssywall.js
@@ -1,45 +1,9 @@
 ﻿/************************************
 * app name:         yssywall饮水思源涂鸦墙
 * author:           i@scaret.in
-* last edit:        2013-3-17
+* last edit:        2013-03-23
 * version:          2.0
 ************************************/
-var config={
-	board:"juhui",
-	load_interval:5000,
-	tips_number:4,
-	superman:["Juhui"],
-	wall_interval:1000,
-	clearInterval_obj:null,
-	clearInterval_wall:null,
-	init_page:500,
-	page:500,
-	include:0,
-	pretty:1,
-	pageNumber:5,
-	boardHandler:"boardHandler",
-	articleHandler:"articleHandler",
-	page_return:500
-	
-}
-
-
-
-function loadBoard(board,page){
-	var url = api.board(board,page);
-	$hackscope.posts = [];
-		
-	$hackhttp.get(url).success(function(data) {
-		for(var i = 0 ; i < data.articles.length ; i++){
-			$scope.posts.push({
-				author:data["articles"][i]["author"],
-				title:data["articles"][i]["link"],
-				content:data["articles"][i]["title"]
-			});
-		}
-	});
-}
-
 
 function postsController($scope,$http,$timeout){
 	$scope.postsShown=[];
