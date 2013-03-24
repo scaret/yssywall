e4de084cbe4da42458d8062fd0614487717e02a4
scaret <lyc945@gmail.com>
2013-03-24 18:06:43 +0800

    加了一个计数器

--- js/yssywall.js
+++ js/yssywall.js
@@ -4,34 +4,61 @@
 * last edit:        2013-03-23
 * version:          2.0
 ************************************/
+function filterTag(text){
+	  if (text) {
+		return text.
+		replace(/<[\w\W]+?>/g,'');
+	  }
+	  return '';
+}
 
 function postsController($scope,$http,$timeout){
 	$scope.postsShown=[];
 	$scope.postsShowing = [];
 	$scope.postsToShow = [];
+	$scope.untractedPosts = [];
 	$scope.currentPage = 500;
-	(function getPostsToShow() {
+	$scope.lastFileId = 0;
+	
+	(function getUntractedPost() {
         $http.get(api.board("juhui",$scope.currentPage)).success(function (data) {
 			for(var i = 0 ; i < data.articles.length ; i++){
-				if(data["articles"][i]["mark"] != "   "){
-					$scope.postsToShow.push({
-						author:data["articles"][i]["author"],
-						title:data["articles"][i]["title"],
-						content:data["articles"][i]["title"],
-						mark:data["articles"][i]["mark"] != "   ",
-						page:data["page"]
+				if(data["articles"][i]["mark"] != "   " && parseInt(data["articles"][i]["file_id"]) > $scope.lastFileId){
+					$scope.untractedPosts.push({
+						board:data["board"],
+						file:data["articles"][i]["file"]
 					});
+					$scope.lastFileId =  parseInt(data["articles"][i]["file_id"]);
 				}
 			} 	
-			$timeout(getPostsToShow, 5000);
+			$timeout(getUntractedPost, 5000);
         });
 		$scope.currentPage +=1;
     })();
+	
+	(function getPostsToShow(){
+		if($scope.untractedPosts.length > 0){
+			var thePost = $scope.untractedPosts.shift();
+			$http.get(api.article(thePost["board"],thePost["file"])).success(function (data) {
+				console.log(data);
+				var content = data["content_lines"][4]?data["content_lines"][4]:data["content_lines"][5];
+				content = filterTag(content);
+				if(!content) return;
+				$scope.postsToShow.push({
+					author:data["content"]["author"],
+					title:data["content"]["title"],
+					content:content
+				});
+			});
+		}
+		$timeout(getPostsToShow, 1000);	
+    })();
+	
 	(function updatePostsShowing() {
 		if($scope.postsToShow.length > 0){
 			$scope.postsShowing.push($scope.postsToShow.shift());
 		}
 		while($scope.postsShowing.length > 3) $scope.postsShown.push($scope.postsShowing.shift());
-		$timeout(updatePostsShowing, 1000);
+		$timeout(updatePostsShowing, 2000);
     })();
 }
